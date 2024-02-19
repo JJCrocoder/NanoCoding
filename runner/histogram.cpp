8215d597f848be896669e107039e7ac7e297a33e
JJCrocoder <160405127+JJCrocoder@users.noreply.github.com>
2024-02-19 10:26:47 +0100

    Update histogram.cpp
    THE VECTOR "x" HAS BEEN REPLACED BY "energies"

--- runner/histogram.cpp
+++ runner/histogram.cpp
@@ -30,21 +30,20 @@ int main(int argc, char* argv[]) {
     // "archivo" fstream object creation. so the file, so the file specified by the path given is opened
     fstream archivo("energies/equilibrium_energies.txt"); 
     vector<float> energies;    // "energies" float vector initialization
-    float energy;
-    vector<float> x;
-    while (archivo >> energy) x.push_back(energy);
+    float energy;              // each energy
+    while (archivo >> energy) energies.push_back(energy);
     archivo.close();
 
     // Variables for the histogram
     const int numBins = atoi(argv[1]);
-    float xmax = *max_element(x.begin(), x.end());
-    float xmin = *min_element(x.begin(), x.end());
+    float xmax = *max_element(energies.begin(), energies.end());
+    float xmin = *min_element(energies.begin(), energies.end());
     float xrange = xmax - xmin;
     float binWidth = xrange / numBins;
     vector<int> histogram(numBins, 0);
 
     // Create the histogram
-    for (float value : x) {
+    for (float value : energies) {
         int binIndex = static_cast<int>((value - xmin) / binWidth);
         histogram[binIndex]++;
     }
@@ -53,7 +52,7 @@ int main(int argc, char* argv[]) {
     cout << "Histogram: " <<endl <<endl;
     vector<float> binNormalizedValues(numBins, 0.0);
     for (int i = 0; i < numBins; ++i) {
-        binNormalizedValues[i] = static_cast<float>(histogram[i]) / (x.size() * binWidth);
+        binNormalizedValues[i] = static_cast<float>(histogram[i]) / (energies.size() * binWidth);
 
         // Print the histogram
         cout << "   Bin " << i + 1 << "= " << binNormalizedValues[i] << endl <<endl;
