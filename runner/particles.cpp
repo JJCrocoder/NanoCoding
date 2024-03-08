c7adefa068736a45365fbaa74d4ecf891d8748cc
JJCrocoder <160405127+JJCrocoder@users.noreply.github.com>
2024-03-08 12:15:36 +0100

    FV particles.cpp

--- runner/particles.cpp
+++ runner/particles.cpp
@@ -35,7 +35,7 @@ float DeltaX = (dmax - dmin)/num_bins;
 // Void functons doesn't return anything but you can change an argument inside them
 // In this case, vec is created outside the function, but modified inside of it
 void mic(float * vec, float Lbox) {
-    for (int i = 0; i < 3 ; ++i) {
+    for (int i = 0; i < dim ; ++i) {
         vec[i] -= floorf(0.5 + vec[i]/Lbox)*Lbox; // we apply the mic for each component
     }  
     return;
@@ -45,12 +45,9 @@ void mic(float * vec, float Lbox) {
 float mic_distance(float * Position_part_i, float *Position_part_j, float Lbox) {
     float rij[dim];
     float mod2_rij; 
-    for (int k = 0; k < 3; ++k)
-    {
-        rij[k] = Position_part_j[k] - Position_part_i[k];
-        mic(rij, Lbox)
-        mod2_rij += rij[k]*rij[k];
-    }
+    for (int k = 0; k < dim; ++k) rij[k] = Position_part_j[k] - Position_part_i[k];
+    mic(rij, Lbox);
+    for (int k = 0; k < dim; ++k) mod2_rij += rij[k]*rij[k];
     return sqrt(mod2_rij);
 }
 
