b6a32de8f53d6e358cb15dc033fae8e2a4aa406b
JJCrocoder <160405127+JJCrocoder@users.noreply.github.com>
2024-03-19 19:37:55 +0100

    Density and Temperature deleted parameters.h
    The density and temperature should be added as arguments of the executable, and thus, "Temp" and "Npart" as function arguments

--- runner/parameters.h
+++ runner/parameters.h
@@ -4,5 +4,3 @@ float sigma = 1.0;       // Particle diameter
 float epsilon = 1.0;     // Characteristic energy
 float Lbox = 15*sigma;   // Box length
 float r_cut = 2.5*sigma; // cutoff distance
-float densinput = 0.7;   // Input density
-float Temp = 1.9;        // Temperature of the system
